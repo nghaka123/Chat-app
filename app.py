887f402191414f8fc241597cfd82c10b0309925d
nghaka123 <nghakacoc5@gmail.com>
2026-07-29 12:00:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from supabase import create_client, Client
-import os
-from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials, firestore
+import json, os
 
-load_dotenv()
-app = Flask(__name__)
-app.secret_key = "chatapp123"
-
-# Supabase connect
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-
-@app.route("/")
-def home():
-    return redirect(url_for('login'))
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session['user'] = request.form["email"]
-        return redirect(url_for("chat"))
-    return render_template("login.html")
-
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    return render_template("signup.html")
-
-@app.route("/chat")
-def chat():
-    if 'user' not in session:
-        return redirect(url_for('login'))
-    
-    try:
-        # Test connection
-        test = supabase.table("messages").select("*").limit(1).execute()
-        print("Supabase Connected:", test)
-        messages_data = test.data
-    except Exception as e:
-        print("Supabase Error:", e)  # Render Logs ah a rawn lang ang
-        messages_data = []
-    
-    return render_template("chat.html", user=session['user'], messages=messages_data)
-
-
-@app.route("/send", methods=["POST"])
-def send():
-    if 'user' not in session:
-        return redirect(url_for('login'))
-    
-    try:
-        message = request.form["message"]
-        user = session['user']
-        
-        supabase.table("messages").insert({
-            "user": user,
-            "message": message
-        }).execute()
-        
-    except Exception as e:
-        print("Error:", e)  # Render Logs ah kan en thei
-        return f"Error: {e}"
-    
-    return redirect(url_for("chat"))
-    
-    # Supabase ah save
-    supabase.table("messages").insert({
-        "user": user,
-        "message": message
-    }).execute()
-    
-    return redirect(url_for("chat"))
-
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(url_for("login"))
+key_dict = json.loads(os.environ.get('FIREBASE_KEY'))
+cred = credentials.Certificate(key_dict)
+firebase_admin.initialize_app(cred)
+db = firestore.client()

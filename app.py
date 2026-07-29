27270e8e1b0315a9749c4c2f5cf88037c6de0e0b
nghaka123 <nghakacoc5@gmail.com>
2026-07-29 15:44:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-import firebase_admin
-from firebase_admin import credentials, firestore
-import json, os
-from datetime import datetime
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, session, flash
+import pyrebase
 
 app = Flask(__name__)
-app.secret_key = "secret123_chat_app_key" # heihi thlak danglam thei
+app.secret_key = "supersecretkey123"
 
-# ========== FIREBASE CONNECT ==========
-db = None
-try:
-    firebase_key = os.environ.get('FIREBASE_KEY')
-    if firebase_key:
-        key_dict = json.loads(firebase_key)
-        cred = credentials.Certificate(key_dict)
-        firebase_admin.initialize_app(cred)
-        db = firestore.client()
-        print("Firebase Connected Successfully!")
-    else:
-        print("FIREBASE_KEY not found in Environment Variables")
-except Exception as e:
-    print("Firebase Error:", e)
-# =======================================
+# === HEI HI I FIREBASE CONFIG IN THLAK RAWH ===
+config = {
+  "apiKey": "AIzaSyXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "authDomain": "your-project.firebaseapp.com",
+  "projectId": "your-project",
+  "storageBucket": "your-project.appspot.com

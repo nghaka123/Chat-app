f3ab064a569ccad89ece29909dcc14c3eb4deece
nghaka123 <nghakacoc5@gmail.com>
2026-07-29 13:44:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 import json, os
 
-app = Flask(__name__)
-
 key_dict = json.loads(os.environ.get('FIREBASE_KEY'))
 cred = credentials.Certificate(key_dict)
 firebase_admin.initialize_app(cred)

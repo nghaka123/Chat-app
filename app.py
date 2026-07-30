42efc93c52323ce6b19ddc4e0f2f74131e94cd3b
nghaka123 <nghakacoc5@gmail.com>
2026-07-30 10:52:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,9 @@ def users():
         return redirect('/login')
 
     all_users = db.child("users").get().val()
-    if all_users is None:
+    if all_users is None:  # <-- hetah space 4
+        all_users = {}     # <-- hetah pawh space 4. None a nih chuan dict ruak ah chantir
+
     return render_template('users.html', users=all_users, me=session['user'])
 
 @app.route('/chat/<room_id>', methods=['GET', 'POST'])

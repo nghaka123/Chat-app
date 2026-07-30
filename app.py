f4a06247bed6ab741a26b28753ce344adf81a69f
nghaka123 <nghakacoc5@gmail.com>
2026-07-30 10:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form["email"]
